55093dccc55ce41a146d9d79eb4988f6e7c5cd99
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-03-02 15:11:27 +0200

    my first commit

---
+++ greet.js
@@ -0,0 +1,5 @@
+function Hellofunction(){
+var greetButton = document.getElementById("greet").value;
+document.getElementById("greeting").innerHTML= 'Hello, '+ greetButton;
+var greetButton =document.getElementById('greet').value=''
+};
